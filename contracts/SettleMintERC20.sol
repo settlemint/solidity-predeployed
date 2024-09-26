3582fc05b1f72fbb74802415f7ea0cf399ecf055
Roderik van der Veer <roderik@settlemint.com>
2024-09-26 19:14:02 +0200

    feat: predeploy erc1155

--- contracts/SettleMintERC20.sol
+++ contracts/SettleMintERC20.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.24;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 /// @title A basic ERC20 Token without authorization solution
-/// @dev Implements a basic ERC20 token with minting functionality.
+/// @dev Implements a basic ERC20 token with minting functionality. IMPORTANT: This contract does not have any authorization mechanism. It is intended to be used as a basic token for testing or development purposes.
 /// @custom:security-contact support@settlemint.com
 contract SettleMintERC20 is ERC20 {
     /// @dev Initializes the contract by setting a `name` and a `symbol` to the token.
--- ignition/modules/main.ts
+++ ignition/modules/main.ts
@@ -1,9 +1,10 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 const SettleMintERC20Module = buildModule("SettleMintERC20Module", (m) => {
-  const counter = m.contract("SettleMintERC20");
+  const erc20 = m.contract("SettleMintERC20");
+  const erc1155 = m.contract("SettleMintERC1155");
 
-  return { counter };
+  return { erc20,erc1155 };
 });
 
 export default SettleMintERC20Module;
