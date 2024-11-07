6696ac8640d9f66106e8e2b04541e08ae64d3eaa
Roderik van der Veer <roderik@settlemint.com>
2024-11-07 08:51:52 +0100

    fix: indexing of the pair

--- subgraph/src/fetch/dex.ts
+++ subgraph/src/fetch/dex.ts
@@ -1,67 +1,55 @@
 import { constants } from '@amxx/graphprotocol-utils';
 import {
-  Address
+  Address,
+  BigDecimal
 } from '@graphprotocol/graph-ts';
 import { Account, ERC20DexPair, ERC20DexStake } from '../../generated/schema';
 import { StarterKitERC20Dex } from '../../generated/templates/StarterKitERC20Dex/StarterKitERC20Dex';
 
 export function fetchDex(address: Address): ERC20DexPair {
   let pair = ERC20DexPair.load(address)
-
   let endpoint = StarterKitERC20Dex.bind(address)
-  let baseToken = endpoint.try_baseToken()
-  let quoteToken = endpoint.try_quoteToken()
 
   if (pair === null) {
     pair = new ERC20DexPair(address)
-
-    let name = endpoint.try_name()
-    let symbol = endpoint.try_symbol()
-    let decimals = endpoint.try_decimals()
-
-    pair.name = name.reverted ? null : name.value
-    pair.symbol = symbol.reverted ? null : symbol.value
-    pair.decimals = decimals.reverted ? 18 : decimals.value
+    pair.name = endpoint.name()
+    pair.symbol = endpoint.symbol()
+    pair.decimals = endpoint.decimals()
     pair.asAccount = address
   }
 
-  let baseReserve = endpoint.try_getBaseTokenBalance()
-  let quoteReserve = endpoint.try_getQuoteTokenBalance()
-  let totalSupply = endpoint.try_totalSupply()
-  let swapFee = endpoint.try_swapFee()
+  let baseReserve = endpoint.getBaseTokenBalance()
+  let quoteReserve = endpoint.getQuoteTokenBalance()
 
-  pair.baseToken = baseToken.reverted ? constants.ADDRESS_ZERO : baseToken.value
-  pair.quoteToken = quoteToken.reverted ? constants.ADDRESS_ZERO : quoteToken.value
-  pair.swapFee = swapFee.reverted ? constants.BIGINT_ZERO : swapFee.value
+  pair.baseToken = endpoint.baseToken()
+  pair.quoteToken = endpoint.quoteToken()
+  pair.swapFee = endpoint.swapFee()
 
-  pair.baseReserveExact = baseReserve.reverted ? constants.BIGINT_ZERO : baseReserve.value
-  pair.baseReserve = baseReserve.reverted
-    ? constants.BIGDECIMAL_ZERO
-    : baseReserve.value.toBigDecimal()
+  pair.baseReserveExact = baseReserve
+  pair.baseReserve = baseReserve.toBigDecimal()
+  pair.quoteReserveExact = quoteReserve
+  pair.quoteReserve = quoteReserve.toBigDecimal()
 
-  pair.quoteReserveExact = quoteReserve.reverted ? constants.BIGINT_ZERO : quoteReserve.value
-  pair.quoteReserve = quoteReserve.reverted
-    ? constants.BIGDECIMAL_ZERO
-    : quoteReserve.value.toBigDecimal()
-
-  pair.totalSupplyExact = totalSupply.reverted ? constants.BIGINT_ZERO : totalSupply.value
-  pair.totalSupply = totalSupply.reverted
-    ? constants.BIGDECIMAL_ZERO
-    : totalSupply.value.toBigDecimal()
+  let totalSupply = endpoint.totalSupply()
+  pair.totalSupplyExact = totalSupply
+  pair.totalSupply = totalSupply.toBigDecimal()
 
   // Calculate prices based on reserves
-  if (!baseReserve.reverted && !quoteReserve.reverted && quoteReserve.value.gt(constants.BIGINT_ZERO)) {
-    pair.baseTokenPrice = quoteReserve.value.toBigDecimal()
-      .div(baseReserve.value.toBigDecimal())
-    pair.quoteTokenPrice = baseReserve.value.toBigDecimal()
-      .div(quoteReserve.value.toBigDecimal())
+  if (quoteReserve.gt(constants.BIGINT_ZERO) && baseReserve.gt(constants.BIGINT_ZERO)) {
+    // Need to adjust for decimals of both tokens
+    let baseDecimals = BigDecimal.fromString('1' + '0'.repeat(endpoint.decimals()))
+    let quoteDecimals = BigDecimal.fromString('1' + '0'.repeat(endpoint.decimals()))
+
+    pair.baseTokenPrice = quoteReserve.toBigDecimal().div(quoteDecimals)
+      .div(baseReserve.toBigDecimal().div(baseDecimals))
+    pair.quoteTokenPrice = baseReserve.toBigDecimal().div(baseDecimals)
+      .div(quoteReserve.toBigDecimal().div(quoteDecimals))
   } else {
-    pair.baseTokenPrice = constants.BIGDECIMAL_ONE
-    pair.quoteTokenPrice = constants.BIGDECIMAL_ONE
+    pair.baseTokenPrice = constants.BIGDECIMAL_ZERO
+    pair.quoteTokenPrice = constants.BIGDECIMAL_ZERO
   }
 
   pair.save()
-
   return pair as ERC20DexPair
 }
 
